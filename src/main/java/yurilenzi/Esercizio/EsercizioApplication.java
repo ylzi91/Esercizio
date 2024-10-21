a52ec7c39aceb7c951e529f2cac6d0f10a89fe6d
ylzi91 <97354085+ylzi91@users.noreply.github.com>
2024-10-21 14:21:27 +0200

    Inizio

---
+++ src/main/java/yurilenzi/Esercizio/EsercizioApplication.java
@@ -0,0 +1,13 @@
+package yurilenzi.Esercizio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yurilenzi/Esercizio/EsercizioApplicationTests.java
@@ -0,0 +1,13 @@
+package yurilenzi.Esercizio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

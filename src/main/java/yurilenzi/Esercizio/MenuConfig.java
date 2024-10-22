26076bddea8415043c16079659f0fc75e3c5e805
ylzi91 <97354085+ylzi91@users.noreply.github.com>
2024-10-22 16:08:05 +0200

    Aggiunta Prototype

--- src/main/java/yurilenzi/Esercizio/MenuConfig.java
+++ src/main/java/yurilenzi/Esercizio/MenuConfig.java
@@ -2,6 +2,7 @@ package yurilenzi.Esercizio;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import yurilenzi.Esercizio.entities.*;
 
 import java.time.LocalDateTime;
@@ -12,41 +13,49 @@ import java.util.List;
 public class MenuConfig {
 
     @Bean
+    @Scope("prototype")
     public Toppings cheese() {
         return new Toppings(92, 0.69, "Cheese");
     }
 
     @Bean
+    @Scope("prototype")
     public Toppings ham() {
         return new Toppings(35, 0.99, "Ham");
     }
 
     @Bean
+    @Scope("prototype")
     public Toppings onions() {
         return new Toppings(22, 0.69, "Onions");
     }
 
     @Bean
+    @Scope("prototype")
     public Toppings pineapple() {
         return new Toppings(24, 0.79, "Pineapple");
     }
 
     @Bean
+    @Scope("prototype")
     public Toppings salami() {
         return new Toppings(86, 0.99, "Salami");
     }
 
     @Bean
+    @Scope("prototype")
     public Drink lemonade() {
         return new Drink("Lemonade", 128, 1.29, 0.33, false);
     }
 
     @Bean
+    @Scope("prototype")
     public Drink water() {
         return new Drink("Water", 0, 1.29, 0.5, false);
     }
 
     @Bean
+    @Scope("prototype")
     public Drink wine() {
         Drink drink = new Drink("Wine", 607, 7.49, 0.75, true);
         drink.setPercentage(13);
@@ -54,6 +63,7 @@ public class MenuConfig {
     }
 
     @Bean
+    @Scope("prototype")
     public Pizza pizzaMarg() {
         Pizza marg = new Pizza("Pizza Margherita", 1104, 4.99);
         marg.addTopping(cheese());
@@ -61,6 +71,7 @@ public class MenuConfig {
     }
 
     @Bean
+    @Scope("prototype")
     public Pizza hawaiian() {
         Pizza haw = new Pizza("Hawaiian Pizza", 1024, 6.49);
         haw.addTopping(cheese());
@@ -70,6 +81,7 @@ public class MenuConfig {
     }
 
     @Bean
+    @Scope("prototype")
     public Pizza salamiPizza() {
         Pizza sal = new Pizza("Salami Pizza", 1024, 6.49);
         sal.addTopping(cheese());
